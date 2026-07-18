a6978bbee62720c290a6770e38e98df6ee356060
solmasta <lukedorsett@hotmail.com>
2026-07-18 05:31:29 +0000

    Add PWA support and fix React loading

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,13 @@
-const CACHE_NAME = 'mts-assistant-v1';
-
-self.addEventListener('install', event => {
+const CACHE_NAME = "mts-assistant-v1";
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(['./']))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(["./"]))
   );
 });
-
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
